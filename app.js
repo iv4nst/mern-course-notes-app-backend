b41930e16c7fd766d33fec84a93f990788ae814f
Ivan Stankovic <stankovicc.ivan@gmail.com>
2024-01-08 16:11:53 +0100

    starting backend app

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/notes', (req, res) => {
+    const notes = [
+        {
+            text: 'Note 1',
+            link: 'some link 1...',
+        },
+        {
+            text: 'Note 2',
+            link: 'some link 2...',
+        },
+    ]
+    res.json({ notes })
+})
 
 const port = process.env.PORT || 3000
 
