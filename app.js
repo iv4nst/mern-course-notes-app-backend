592ecf7be74b874dfcda6f74c1f3f74b96c0772d
Ivan Stankovic <stankovicc.ivan@gmail.com>
2024-01-17 16:53:34 +0100

    add router for notes

--- app.js
+++ app.js
@@ -4,26 +4,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const notesRouter = require('./api/v1/index')
+
 // use "cors" only in DEV to allow access from everywhere (in PROD, whitelist domains)
 app.use(cors())
 
+// root (/)
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.get('/notes', (req, res) => {
-    const notes = [
-        {
-            text: 'Note 1',
-            link: 'some link 1...',
-        },
-        {
-            text: 'Note 2',
-            link: 'some link 2...',
-        },
-    ]
-    res.json({ notes })
-})
+// use routers
+app.use('/notes', notesRouter)
 
 const port = process.env.PORT || 3000
 
